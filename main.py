af5fd39c36d14664a10a5ef6a4002380f72dfc20
Mr-afk-cell <kboxser2026@kehillahstudent.org>
2025-03-19 12:12:54 -0700

    assignment

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ Every person's unique talents are valued equally.
 Everyone takes responsibility for themselves and their community."""
 
 # Make a dictionary where the keys are characters and the values are the number of times that character appears in the `text` print the dicitonary
+letter_counts = {}
+for letter in text:
+    if letter in letter_counts:
+       letter_counts[letter] +=1
+    else: 
+        letter_counts[letter] =1
+print(letter_counts)
+
